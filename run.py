5ff11a711017e2a9be511c3347fcb90a9c8bd97d
Bogdan Prăzniciuc <bogdan@onis.studio>
2020-03-01 10:35:24 +0000

    Basic view added

--- run.py
+++ run.py
@@ -3,11 +3,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "<h1>Hello there</h1>"
 
-if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=True)
+@app.route("/")
+def index():
+    """Main page with instructions"""
+    return "To send a message use: /USERNAME/MESSAGE"
+
+
+@app.route("/<username>")
+def user(username):
+    return "Hi " + username
+
+
+@app.route("/<username>/<message>")
+def send_message(username, message):
+    return "{0}: {1}".format(username, message)
+
+
+app.run(host=os.getenv("IP"), port=os.getenv("PORT"), debug=True)
